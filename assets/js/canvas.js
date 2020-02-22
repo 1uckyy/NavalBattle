1cb3d7ccb425c25057192c2dbea2c357dbe434c0
Vladislav Novak <novak.vlad97@mail.ru>
2020-02-22 08:07:56 +0300

    Added three cells ship

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -3,6 +3,8 @@ var context = canvas.getContext("2d");
 
 //supporting array
 var infoCells = [];
+//possible cells
+var possibleCells = [];
 
 //drawing settings
 context.lineWidth = 3;
@@ -106,14 +108,62 @@ randBtn.onclick = function() {
     }
 
     //THREE CELLS SHIP
+    //refresh possible cells
+    possibleCells = [];
     //axis selection
     axis = randomInteger(0,1);
-    //possible cells
-    var possibleCells = [];
+    //horiz ship
     if(axis==0) {
-        for(var i=0;i<100;i++) {
-
+        //calculating possible cells
+        for(var i=0; i<10; i++)
+        {
+            for(var j=0; j<8; j++)
+            {
+                if(infoCells[i][j] != 1 && infoCells[i][j+1] != 1 && infoCells[i][j+2] != 1)
+                    possibleCells.push(i+""+j);
+            }
         }
-    }
 
+        var rndCell = randomInteger(0,possibleCells.length-1);
+        var xy = possibleCells[rndCell].split('');
+        var x = parseInt(xy[0],10);
+        var y = parseInt(xy[1],10);
+
+        //drawimg ship
+        for(var i=0; i<3; i++)
+            context.fillRect(3+33*(y+i),3+33*x,30,30);
+
+        //locked cells
+        for(var i=0; i<5; i++)
+            for(var j=0; j<3; j++)
+                if(!(x==0 && j==0 || x==9 && j==2 || y==0 && i==0 || y==7 && i==5))
+                    infoCells[x-1+j][y-1+i] = 1;
+    }
+    //vert ship
+    else {
+        //calculating possible cells
+        for(var i=0; i<8; i++)
+        {
+            for(var j=0; j<10; j++)
+            {
+                if(infoCells[i][j] != 1 && infoCells[i+1][j] != 1 && infoCells[i+2][j] != 1)
+                    possibleCells.push(i+""+j);
+            }
+        }
+
+        var rndCell = randomInteger(0,possibleCells.length-1);
+        var xy = possibleCells[rndCell].split('');
+        var x = parseInt(xy[0],10);
+        var y = parseInt(xy[1],10);
+
+        //drawimg ship
+        for(var i=0; i<3; i++)
+            context.fillRect(3+33*y,3+33*(x+i),30,30);
+
+        //locked cells
+        for(var j=0; j<5; j++)
+            for(var i=0; i<3; i++)
+                if(!(x==0 && j==0 || x==7 && j==5 || y==0 && i==0 || y==9 && i==2))
+                    infoCells[x-1+j][y-1+i] = 1;
+    }
 }
